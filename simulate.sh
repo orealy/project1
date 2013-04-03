948e55b10c5508d9943d6ba30dcce593c0d9d367
Chris Webb <c.webb@unsw.edu.au>
2013-04-03 11:48:47 +1100

    added log and corrected devise command.

--- simulate.sh
+++ simulate.sh
@@ -7,9 +7,9 @@ DEVISE_SCRIPT="structure.scm"
 BASE_NAME="quad"
 DESSIS_COMMANDS="settings_des.cmd"
 
-devise -l -e $DEVISE_SCRIPT
-mesh -P $BASE_NAME
-dessis $DESSIS_COMMANDS
+devise -e -l $DEVISE_SCRIPT > "simulate.log"
+mesh -P $BASE_NAME >> "simulate.log"
+dessis $DESSIS_COMMANDS >> "simulate.log"
 
 echo "Done!"
-echo "Run techplot_sie $(BASE_NAME)_des.dat $(BASE_NAME)_msh.grd to view."
+echo "Run techplot_sie ${BASE_NAME}_des.dat ${BASE_NAME}_msh.grd to view."
